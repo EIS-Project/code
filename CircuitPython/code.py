051792cf9293c36563e907ecfd41b87a24fcf5a4
conrad2001 <conrad93397215@gmail.com>
2021-11-27 13:05:31 -0800

    moved adg725 and added mux disable

--- CircuitPython/code.py
+++ CircuitPython/code.py
@@ -2,61 +2,27 @@
 AD725 analog mux switch using SPI
 graphical datasheet: https://cdn.sparkfun.com/assets/learn_tutorials/4/5/4/graphicalDatasheet-Dev.pdf
 """
-import board
-import busio
-import digitalio
-import time
-import supervisor 
-
+from lib.ADG725 import ADG725
+from lib.LED import led
+import supervisor
 
 def main():
-
-    cs = digitalio.DigitalInOut(board.D10)
-    cs.switch_to_output(value=True)
-    # cs.direction = digitalio.Direction.OUTPUT
-    cs.value = True
-
-
-
-    led = digitalio.DigitalInOut(board.A5)
-    led.direction = digitalio.Direction.OUTPUT
-
-    led.value = True
-
-    print('begin')
+    led()   # initialize led
+    mux = ADG725()  # initialize switch
+    print('ready')
+    
     while True:
-        if supervisor.runtime.serial_bytes_available:
-            
-            
-            data = input('').strip()
-            switch_channel(int(data), cs)
-            print('ok, switched to DUT ', data)
-            
-        else:
-            led.value = False
+        if supervisor.runtime.serial_bytes_available:   # if received data from serial bus
+            data = int(input('').strip())
+            if data <= 0:
+                mux.disable()
+                print('all switches turned off')
+            else: 
+                mux.switch_channel(data)   # get channel no. and switch channel, return true for success, false for fail
+                print('ok, switched to channel ', data)
+                # blink(data, led)
 
-def blink(x, led):
-    for i in range(x*2):
-        led.value ^= True
-        time.sleep(0.5)
-
-def switch_channel(channel, cs):
-    EN = 7
-    CSA = 6
-    CSB = 5
-    DATA = 0x00
-    DATA &= ~( 1 << EN | 1 << CSA | 1 << CSB)
-    DATA |= ((channel-1) & 0x0F)
-    buf = bytearray(1)
-    buf[0] = DATA
-
-    with busio.SPI(clock=board.D13, MOSI=board.D11) as spi:
-        while not spi.try_lock():
-            pass
-        spi.configure(baudrate=5000000, phase=1, polarity=0)
-        cs.value = False
-        spi.write(buf)
-        cs.value = True
+            
 
 if __name__ == '__main__':
     main()
